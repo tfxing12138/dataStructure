19b2649c26feb7c062f6ec98c6ebb5d6c61df9b3
tanfuxing <t18370240624@163.com>
2023-01-29 10:59:27 +0800

    项目初始化

---
+++ src/main/java/com/example/demo/DataStructureApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructureApplication.class, args);
+        System.out.println("启动成功！！");
+    }
+
+}
