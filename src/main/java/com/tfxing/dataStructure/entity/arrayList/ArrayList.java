e956e01e122243861878ef1f5374f2799d0fcc17
tanfuxing <t18370240624@163.com>
2023-01-29 18:30:17 +0800

    arraylist

--- src/main/java/com/tfxing/dataStructure/entity/arrayList/ArrayList.java
+++ src/main/java/com/tfxing/dataStructure/entity/arrayList/ArrayList.java
@@ -2,6 +2,10 @@ package com.tfxing.dataStructure.entity.arrayList;
 
 import lombok.Data;
 
+import java.util.StringJoiner;
+
+import static java.util.Objects.isNull;
+
 /**
  * @author :tanfuxing
  * @date :2023/1/29
@@ -9,6 +13,115 @@ import lombok.Data;
  */
 @Data
 public class ArrayList<T> {
+
+    /**
+     * 列表长度
+     */
+    private Integer size = 0;
+
+    /**
+     * 容量
+     */
+    private Integer capacity = 10;
     
+    Object[] array = new Object[capacity];
+
+    /**
+     * 添加
+     * @param t
+     */
+    public void add(T t) {
+        if(isFull()) {
+            enlarge();
+        }
+        if(isEmpty(array))size++;
+        array[size] = t;
+    }
+
+    /**
+     * 是否为空
+     * @param array
+     * @return
+     */
+    private boolean isEmpty(Object[] array) {
+        return null == array[0];
+    }
+
+    /**
+     * 扩容
+     */
+    private void enlarge() {
+        capacity = Math.toIntExact(Math.round(capacity * 1.5));
+        Object[] tempArray = new Object[capacity];
+        copy(array,tempArray);
+        array = tempArray;
+    }
+
+    /**
+     * 复制
+     * @param array
+     * @param tempArray
+     */
+    private void copy(Object[] array, Object[] tempArray) {
+        for (int i = 0; i < array.length; i++) {
+            tempArray[i] = array[i];
+        }
+    }
+
+    /**
+     * 容量是否已满
+     * @return
+     */
+    private boolean isFull() {
+        return size < capacity;
+    }
+
+    /**
+     * 删除
+     * @param index
+     */
+    public void remove(int index) {
+        //校验是否下标越界
+        checkOutFlow(index);
+        //压缩整理
+        compress(index);
+        size--;
+    }
+
+    /**
+     * 检测是否下标越界
+     * @param index
+     */
+    private void checkOutFlow(int index) {
+        if(index >= size || index < 0) {
+            String message = "ArrayIndexOutOfBoundsException,size:%s,index:%s";
+            message = String.format(message,size,index);
+            throw new RuntimeException(message);
+        }
+    }
+
+    /**
+     * 压缩整理
+     * @param index
+     */
+    private void compress(int index) {
+        if(index == size) {
+            array[index+1] = null;
+        }
+        for (Integer i = index+1; i < size; i++) {
+            array[i] = array[i+1];
+            array[i+1] = null;
+        }
+    }
     
+    public String toString() {
+        StringJoiner str = new StringJoiner(",");
+        for (Object o : array) {
+            if(isNull(o)) {
+                continue;
+            }
+            str.add(o.toString());
+        }
+        return "["+str+"]";
+    }
 }
--- src/test/java/com/tfxing/dataStructure/DemoApplicationTests.java
+++ src/test/java/com/tfxing/dataStructure/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.tfxing.dataStructure;
 
+import com.tfxing.dataStructure.entity.arrayList.ArrayList;
 import com.tfxing.dataStructure.entity.linkedList.LinkedList;
 import com.tfxing.dataStructure.entity.rbTree.RBTree;
 import org.junit.jupiter.api.Test;
@@ -43,5 +44,43 @@ class DemoApplicationTests {
         System.out.println();
         
         System.out.println(list.getSize());
+    }
+    
+    @Test
+    void testArrayList() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        System.out.println(list.getSize());
+        list.remove(1);
+        list.remove(4);
+        list.remove(7);
+        list.remove(10);
+        System.out.println(list);
+        System.out.println(list.getSize());
+    }
+    
+    @Test
+    void testAdd() {
+        int i = 0;
+        System.out.println(++i);
+        
+        int j = 0;
+        System.out.println(j+=1);
+        
+        int g = 0;
+        System.out.println(g++);
     }
 }
